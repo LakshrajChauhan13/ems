159bdd04b5f3a312a4145f77c0ba0cb71656eed6
LakshrajChauhan13 <lakshrajchauhan13@gmail.com>
2025-08-18 04:03:24 +0530

    again commit

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -18,7 +18,7 @@ const Header = ({data}) => {
   return (
     <div className='flex justify-between items-end ' >                                                  { /*data.firstName*/} 
         <h1 className='text-2xl font-semibold '> Hello, <br />  <span className='text-3xl font-bold' > Admin👋 </span> </h1>
-  { /* onclick functionality */ } <button onClick={logoutUser} className=' bg-red-600 px-3 py-1 rounded font-medium text-lg'> Log Out </button>
+        <button onClick={logoutUser} className=' bg-red-600 px-3 py-1 rounded font-medium text-lg'> Log Out </button>
     </div>
   )
 }
